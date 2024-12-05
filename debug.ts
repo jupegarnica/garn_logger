4c40ffa8c594091012d1f90c6ab002cd802e13fe
Juan Garnica <juan@harbiz.io>
2024-12-05 13:04:38 +0100

    refactor: update better console method to use level instead of setLevel in debug.ts

--- debug.ts
+++ debug.ts
@@ -1,25 +1,25 @@
 import { better } from "./main.ts";
-better(console).setLevel("error");
+better(console).level("error");
 
 console.error("1 TEST This is a test error");
 console.warn("2 TEST NOT this should be logged");
 console.info("3 TEST NOT this should be logged");
 console.log("4 TEST NOT this should be logged");
 
-better(console).setLevel("warn");
+better(console).level("warn");
 console.error("5 TEST This should be logged");
 console.warn("6 TEST This should be logged");
 console.info("7 TEST NOT this should be logged");
 console.log("8 TEST NOT this should be logged");
 
-better(console).setLevel("info");
+better(console).level("info");
 console.error("9 TEST This should be logged");
 console.warn("10 TEST This should be logged");
 console.info("11 TEST This should be logged");
 console.log("12 TEST NOT this should be logged");
 
 
-better(console).setLevel("debug");
+better(console).level("debug");
 console.error("13 TEST This should be logged");
 console.warn("14 TEST This should be logged");
 console.info("15 TEST This should be logged");
